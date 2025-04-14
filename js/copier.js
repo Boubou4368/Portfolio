486e765b286bb844d838dca50278bd0435c4da78
boubou4368 <liaatherly@gmail.com>
2025-04-14 13:09:31 +0200

    ajout des fonctions de copie

--- js/copier.js
+++ js/copier.js
@@ -1,10 +1,16 @@
-function copierEmail() {
-    var email = document.getElementById('email').innerText;
+function copier(br) {
+    var contenue = document.querySelector(`.case[data-br="${br}"] p`);
 
-    // Utilisation de l'API Clipboard pour copier le texte
-    navigator.clipboard.writeText(email).then(function() {
-        alert('copié dans le presse papier: ' + email);
-    }).catch(function(err) {
-        console.error('Erreur lors de la copie : ', err);
-    });
+    if (contenue) {
+        var texteACopier = contenue.textContent;
+
+        // Utilisation de l'API Clipboard pour copier le texte
+        navigator.clipboard.writeText(texteACopier).then(function() {
+            alert('Copié dans le presse-papier: ' + texteACopier);
+        }).catch(function(err) {
+            console.error('Erreur lors de la copie : ', err);
+        });
+    } else {
+        console.error('Élément non trouvé pour data-br="' + br + '"');
+    }
 }
