a079f9e9758ca531c9daae01845d4fd9d5d7a138
boubou4368 <liaatherly@gmail.com>
2025-04-15 23:25:30 +0200

    modification des "voir plus" pour le responsive et ajout du logo en haut a gauche

--- js/viewmore.js
+++ js/viewmore.js
@@ -1,15 +1,14 @@
 function readMore(nr) {
-  let p = document.querySelector(`.card[data-nr="${nr}"] p`);
+  let p = document.querySelector(`.card[data-nr="${nr}"] .viewmore`);
   let btnText = document.querySelector(`.card[data-nr="${nr}"] .myBtn`);
-  let t = document.querySelector(`.card[data-nr="${nr}"] .viewmore`)
-
 
   if (!p.classList.contains('collapse')) {
     p.classList.add('collapse');
+    p.style.maxHeight = null; // Réinitialiser la hauteur maximale
     btnText.innerHTML = "Voir plus";
-    //t.innerHTML = "programmation - c++,java,python, javascript balisage - html,css, markdown declaratif -SQL	 systeme d'exploitation -Linux,bash";
   } else {
     p.classList.remove('collapse');
+    p.style.maxHeight = p.scrollHeight + "px"; // Définir la hauteur maximale en fonction du contenu
     btnText.innerHTML = "Voir moins";
   }
 }
